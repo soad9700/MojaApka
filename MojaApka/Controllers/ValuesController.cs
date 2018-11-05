74faf6a85e9e37c936f3f729bc788910f64f2f1c
Filip Lipowiecki <lipowiecki.f@gmail.com>
2018-11-05 21:46:39 +0100

    initial

---
+++ MojaApka/Controllers/ValuesController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using MojaApka.Domains;
+using MojaApka.Nowy_folder;
+
+namespace MojaApka.Controllers
+{
+    [Route("api/[controller]")]
+    public class ValuesController : Controller
+    {
+        private readonly MojaApkaContext _context;
+
+        public ValuesController(MojaApkaContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet("students/{id}")]
+        public ActionResult<string> Get(int id)
+        {
+            return Json(_context.Students.SingleOrDefault(x => x.Id == id));
+        }
+
+        [HttpPost ("students")]
+        public IActionResult Post([FromBody] CreateStudent createStudent)
+        {
+            var student = new Student() {Name = createStudent.Name}; 
+            _context.Students.Add(student);
+            _context.SaveChanges();
+            return StatusCode(201);
+        }
+    }
+}
---
+++ MojaApka/Nowy folder/MojaApkaContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MojaApka.Domains;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MojaApka.Nowy_folder
+{
+    public class MojaApkaContext : DbContext
+    {
+        public DbSet<Student> Students { get; set; }
+        public MojaApkaContext(DbContextOptions<MojaApkaContext> options) : base(options) { }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder) { }
+    }
+}
