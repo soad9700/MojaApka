87b0ec720daa3e250b59e05bd718866fa37d27ba
Filip Lipowiecki <lipowiecki.f@gmail.com>
2018-11-06 08:23:16 +0100

    Data folder

--- MojaApka/Controllers/ValuesController.cs
+++ MojaApka/Controllers/ValuesController.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using MojaApka.Domains;
-using MojaApka.Nowy_folder;
+using MojaApka.Data;
 
 namespace MojaApka.Controllers
 {
--- MojaApka/Startup.cs
+++ MojaApka/Startup.cs
@@ -6,7 +6,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using MojaApka.Nowy_folder;
+using MojaApka.Data;
 
 namespace MojaApka
 {
